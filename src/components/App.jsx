20a66c23df9e46c11aa4af128bec7c0e0e649656
Nguyễn Văn Kiệt <vankiet.kn78@gmail.com>
2021-07-10 14:47:24 +0700

    clean dist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
-import React from "react";
-import ".././styles/index.scss";
-import Recipes from "./Recipes";
+import React from 'react';
+import '.././styles/index.scss';
+import Recipes from './Recipes';
+import sword from '../images/swc-sword.png';
+import swordSvg from '../images/sword.svg';
 
 export default function App() {
   return (
@@ -11,6 +13,8 @@ export default function App() {
           <h1>hehehe</h1>
         </section>
       </main>
+      <img src={sword} alt="sword" width="250" />
+      <img src={swordSvg} alt="sword" width="250" />
       <Recipes />
     </>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const mode = process.env.NODE_ENV || "development";
+const mode = process.env.NODE_ENV || 'development';
 // khi la prodution thi xuat css dung theo trinh duyet ho tro
 // Temporary workaround for 'browserslist' bug that is being patched in the near future
-let target = process.env.NODE_ENV === "production" ? "browserslist" : "web";
+let target = process.env.NODE_ENV === 'production' ? 'browserslist' : 'web';
 module.exports = {
   // mode defaults to 'production' if not set
   mode: mode,
@@ -11,16 +14,47 @@ module.exports = {
   target: target,
   // entry not required if using `src/index.js` default
   // output not required if using `dist/main.js` default
-
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[hash][ext][query]',
+  },
   module: {
     rules: [
       {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        /**
+         * The `type` setting replaces the need for "url-loader"
+         * and "file-loader" in Webpack 5.
+         *
+         * setting `type` to "asset" will automatically pick between
+         * outputing images to a file, or inlining them in the bundle as base64
+         * with a default max inline size of 8kb
+         */
+        type: 'asset',
+
+        /**
+         * If you want to inline larger images, you can set
+         * a custom `maxSize` for inline like so:
+         */
+        // parser: {
+        //   dataUrlCondition: {
+        //     maxSize: 30 * 1024,
+        //   },
+        // },
+      },
+      {
         test: /\.(s[ac]|c)ss$/i,
         use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
+          {
+            loader: MiniCssExtractPlugin.loader,
+            // This is required for asset imports in CSS, such as url()
+            options: { publicPath: '' },
+          },
+          'css-loader',
+          'postcss-loader',
+          // according to the docs, sass-loader should be at the bottom, which
+          // loads it first to avoid prefixes in your sourcemaps and other issues.
+          'sass-loader',
         ],
       },
       {
@@ -28,22 +62,28 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           // without additional settings, this will reference .babelrc
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
     ],
   },
 
   // This plugin extracts CSS into separate files
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
   },
-  devtool: "source-map",
+  devtool: 'source-map',
 
   // required if using webpack-dev-server
   devServer: {
-    contentBase: "./dist",
+    contentBase: './dist',
     hot: true,
   },
 };
